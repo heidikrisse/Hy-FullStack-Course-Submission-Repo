a43c86c7228cd002c55fca28762cd2a24928314e
heidikrisse <heidi.kristiina90@gmail.com>
2024-09-26 13:13:08 +0300

    osa2: 2.2 kurssitiedot step7 - Add total exercises count to Course component - Add 'Redux' to parts - Display total of exercises in bold

--- osa2/kurssitiedot_2/src/App.jsx
+++ osa2/kurssitiedot_2/src/App.jsx
@@ -1,9 +1,22 @@
-const Course = ({ course }) => (
-  <div>
-    <Header courseName={course.name} />
-    <Content parts={course.parts} />
-  </div>
-)
+const Total = ({ total }) => {
+  return (
+    <p>
+      <b>total of {total} exercises</b>
+    </p>
+  )
+}
+
+const Course = ({ course }) => {
+  const totalExercises = course.parts.reduce((sum, part) => sum + part.exercises, 0)
+
+  return (
+    <div>
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+      <Total total={totalExercises} />
+    </div>
+  )
+}
 
 const Header = ({ courseName }) => <h1>{courseName}</h1>
 
@@ -29,6 +42,7 @@ const App = () => {
       { name: "Fundamentals of React", exercises: 10, id: 1 },
       { name: "Using props to pass data", exercises: 7, id: 2 },
       { name: "State of a component", exercises: 14, id: 3 },
+      { name: "Redux", exercises: 11, id: 4},
     ],
   }
 
