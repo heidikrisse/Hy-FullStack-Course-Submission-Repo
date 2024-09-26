0272d1d0682bff52585a9c206a137ca67481310a
heidikrisse <heidi.kristiina90@gmail.com>
2024-09-26 13:29:20 +0300

    osa2: 2.3 kurssitiedot step8 - console.log total exercises calculation (using reduce method in   calculation as before)

--- osa2/kurssitiedot_2/src/App.jsx
+++ osa2/kurssitiedot_2/src/App.jsx
@@ -7,7 +7,10 @@ const Total = ({ total }) => {
 }
 
 const Course = ({ course }) => {
-  const totalExercises = course.parts.reduce((sum, part) => sum + part.exercises, 0)
+  const totalExercises = course.parts.reduce((sum, part) => {
+    console.log("current sum of exercises:", sum, "\n+ part exercises:", part.exercises, "\n=")
+    return sum + part.exercises
+  }, 0)
 
   return (
     <div>
