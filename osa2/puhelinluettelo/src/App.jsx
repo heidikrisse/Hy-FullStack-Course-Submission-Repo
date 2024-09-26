d17e76ee60456fdc9effbd8abcb0aa335c4feb2b
heidikrisse <heidi.kristiina90@gmail.com>
2024-09-26 16:54:12 +0300

    osa2: 2.7 puhelinluettelo
    - Prevent adding duplicate names to the phonebook, with alert

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -12,10 +12,14 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault()
-    const newPerson = { name: newName }
-    setPersons(persons.concat(newPerson))
-    setNewName('')
-  }
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      const newPerson = { name: newName }
+      setPersons(persons.concat(newPerson))
+      setNewName('')
+    }
+  }  
 
   return (
     <div>
