b72d3bee5dbaa0168a5c60d52cb89fc0d3e8c42e
heidikrisse <heidi.kristiina90@gmail.com>
2024-11-08 12:57:08 +0200

    osa 2: 2.11 puhelinluettelo step6 - Add JSON server running on port 3001 - Configure React app to fetch data from JSON server - Implement data fetching with useEffect using Axios

---
+++ osa2/puhelinluettelo_step6_using_cra/src/App.js
@@ -0,0 +1,99 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Filter = ({ searchTerm, handleSearchChange }) => (
+  <div>
+    filter shown with <input value={searchTerm} onChange={handleSearchChange} />
+  </div>
+)
+
+const PersonForm = ({ addPerson, newName, handleNameChange, newNumber, handleNumberChange }) => (
+  <form onSubmit={addPerson}>
+    <div>
+      name: <input value={newName} onChange={handleNameChange} />
+    </div>
+    <div>
+      number: <input value={newNumber} onChange={handleNumberChange} />
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>
+)
+
+const Persons = ({ personsToShow }) => (
+  <div>
+    {personsToShow.map((person) => (
+      <div key={person.id}>
+        {person.name} {person.number}
+      </div>
+    ))}
+  </div>
+)
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [searchTerm, setSearchTerm] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      const newPerson = { name: newName, number: newNumber }
+      setPersons(persons.concat(newPerson))
+      setNewName('')
+      setNewNumber('')
+    }
+  }
+
+  const personsToShow = persons.filter(person => 
+    person.name.toLowerCase().includes(searchTerm.toLowerCase())
+  )
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <Filter searchTerm={searchTerm} handleSearchChange={handleSearchChange} />
+
+      <h3>Add a new</h3>
+
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+
+      <h3>Numbers</h3>
+
+      <Persons personsToShow={personsToShow} />
+    </div>
+  )
+}
+
+export default App
