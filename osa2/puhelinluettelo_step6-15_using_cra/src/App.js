55a162035f9481483b82528018a6c87e61333507
heidikrisse <heidi.kristiina90@gmail.com>
2024-11-10 20:10:13 +0200

    osa 2: 2.12 puhelinluettelo step 7 - Synchronize adding new numbers with the server

--- osa2/puhelinluettelo_step6-15_using_cra/src/App.js
+++ osa2/puhelinluettelo_step6-15_using_cra/src/App.js
@@ -59,13 +59,18 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault()
+    const newPerson = { name: newName, number: newNumber }
+
     if (persons.some((person) => person.name === newName)) {
       alert(`${newName} is already added to phonebook`)
     } else {
-      const newPerson = { name: newName, number: newNumber }
-      setPersons(persons.concat(newPerson))
-      setNewName('')
-      setNewNumber('')
+      axios
+        .post('http://localhost:3001/persons', newPerson)
+        .then(response => {
+          setPersons(persons.concat(response.data));
+          setNewName('')
+          setNewNumber('')
+        });
     }
   }
 
