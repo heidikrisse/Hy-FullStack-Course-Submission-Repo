3d029ecf1ea97175d4a6a3c6a8ce4d6be92e1bbf
heidikrisse <heidi.kristiina90@gmail.com>
2024-11-27 02:18:51 +0200

    osa 3: 3.2 puhelinluettelon backend step2
    - Info page for the phonebook

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -12,7 +12,17 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+  const date = new Date()
+  const info = `
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${date}</p>
+    `
+  res.send(info)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+  
